b3801e361832ef272e45f6df5ac259e3b5f32c43
Marcin Brandys <marcinbrandys95@gmail.com>
2019-01-03 15:49:27 +0100

    [Schema] Added 3 components, app routing, header, handled google maps, overrided bootstrap colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import {RouterModule, Routes} from '@angular/router';
+import {AgmCoreModule} from '@agm/core';
+
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'info', component: AboutComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCAYMFDnaJ1uoPxeENwACjioCIFOdepaxw'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
