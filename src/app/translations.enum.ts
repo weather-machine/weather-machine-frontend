ccf3747f1d817be21e8ce8ec1c42a70fcb44b91f
Marcin Brandys <marcinbrandys95@gmail.com>
2019-01-03 15:49:27 +0100

    [Schema] Added 3 components, app routing, header, handled google maps, overrided bootstrap colors

---
+++ src/app/translations.enum.ts
@@ -0,0 +1,10 @@
+export enum Translations {
+  NAV_DASHBOARD = 'Strona główna',
+  NAV_INFO = 'O nas',
+  NAV_CONTACT = 'Kontakt',
+
+  DASHBOAD_CHECK_WEATHER = 'Sprawdź pogodę',
+  DASHBOAD_CHECK_WEATHER_DESC = 'Wyszukaj prognozy na najbliższe dni dzięki naszej aplikacji!',
+  DASHBOARD_TYPE_PLACE_NAME = 'Wpisz swoją miejscowość',
+  DASHBOARD_LOCALIZE = 'Lokalizuj'
+}
