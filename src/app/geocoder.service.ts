9b21f4e2116dfd827a9ef5b33b8cfee545797f3f
Marcin Brandys <marcinbrandys95@gmail.com>
2019-01-06 21:06:27 +0100

    Google Maps Markers and Geocoding

--- src/app/geocoder.service.ts
+++ src/app/geocoder.service.ts
@@ -16,11 +16,28 @@ export class GeocoderService {
   geocodeAddress(address: string): Observable<object> {
     return Observable.create((observer: Observer<object>) => {
       this.geocoder.geocode({'address': address}, function (results, status) {
-        // @ts-ignore
-        if (status === 'OK') {
+        if (google.maps.GeocoderStatus.OK) {
           observer.next({
             lat: results[0].geometry.location.lat(),
             lng: results[0].geometry.location.lng()
+          });
+        } else {
+          observer.error('Geocode was not successful for the following reason: ' + status);
+        }
+        observer.complete();
+      });
+    });
+  }
+
+  geocodeLatLng(position: google.maps.LatLng): Observable<object> {
+    return Observable.create((observer: Observer<object>) => {
+      this.geocoder.geocode({'location': position}, function (results, status) {
+        if (google.maps.GeocoderStatus.OK) {
+          observer.next({
+            formattedPlaceName: results[0].formatted_address,
+            placeName: results[0].formatted_address.normalize('NFD').replace(/[\u0300-\u036f]/g, ''),
+            formattedCountry: results[results.length - 1].formatted_address,
+            country: results[results.length - 1].formatted_address.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
           });
         } else {
           observer.error('Geocode was not successful for the following reason: ' + status);
