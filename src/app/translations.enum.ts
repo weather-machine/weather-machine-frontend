9e40f0903128a4fc1a73bbf2b6e1019751106014
Marcin Brandys <marcinbrandys95@gmail.com>
2019-01-06 19:05:36 +0100

    Refactored code

--- src/app/translations.enum.ts
+++ src/app/translations.enum.ts
@@ -3,14 +3,13 @@ export enum Translations {
   NAV_INFO = 'O nas',
   NAV_CONTACT = 'Kontakt',
 
-  DASHBOAD_CHECK_WEATHER = 'Sprawdź pogodę',
-  DASHBOAD_CHECK_WEATHER_DESC = 'Wyszukaj prognozy na najbliższe dni dzięki naszej aplikacji!',
+  DASHBOARD_CHECK_WEATHER = 'Sprawdź pogodę',
+  DASHBOARD_CHECK_WEATHER_DESC = 'Wyszukaj prognozy na najbliższe dni dzięki naszej aplikacji!',
   DASHBOARD_TYPE_PLACE_NAME = 'Nazwa miejscowości',
   DASHBOARD_LOCALIZE = 'Sprawdź',
-
   DASHBOARD_WEATHER_FOR = 'Pogoda dla',
 
-  SEE_MORE = "Pokaż więcej",
+  SEE_MORE = 'Pokaż więcej',
 
-  FOOTER_COPYRIGHT = "© WeatherMachine. All rights reserved",
+  FOOTER_COPYRIGHT = '© WeatherMachine. All rights reserved'
 }
